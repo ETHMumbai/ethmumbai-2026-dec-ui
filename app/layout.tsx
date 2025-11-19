4a5c2dd3d4f75ac6994f413f4b309881b4830c40
BEST developer <website@ethmumbai.in>
2025-11-19 22:16:35 +0530

    Merge Faq and footer into Hero (#2)
    * chore: FAQ
    
    * chore:Footer
    
    * chore: faq margin
    
    * chore: faq style
    
    ---------
    
    Co-authored-by: Diksha <dikshas2911@gmail.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-
 const inter = Inter({
   subsets: ["latin"],
   display: "swap",
@@ -22,7 +21,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${inter.variable} ${inter.variable} antialiased`}
+        className={`${inter.variable} 
+      ${inter.variable} antialiased`}
       >
         {children}
       </body>
