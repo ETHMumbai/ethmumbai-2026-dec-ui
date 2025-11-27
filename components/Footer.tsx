86ff0a80a56252cd925b2af19e1a0c4ef62ea68e
Diksha <dikshas2911@gmail.com>
2025-11-27 17:27:19 +0530

    choere: footer change

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,5 @@
 "use client";
-import ETHMumbaiFooterLogo from "../public/assets/ethmumbai-logo-white.svg";
+import ETHMumbaiFooterLogo from "../public/assets/ethmumbai-logo-illustrated.svg";
 import Image from "next/image";
 
 import TwitterLogo from "../public/assets/x-logo.png";
@@ -12,109 +12,112 @@ import TelegramWhite from "../public/assets/telegram-white.png";
 const Footer = () => {
   return (
     <footer className="relative w-full bg-[#E2231A] pt-5 px-6 sm:px-8 lg:px-12 text-[14px] leading-6 ">
-      <div className="flex flex-col sm:flex-row sm:justify-between gap-6 w-full py-4">
+      <div className="flex flex-col sm:flex-row sm:justify-between gap-6 w-full py-5">
         {/* 1/2 width on desktop */}
         <div className="sm:w-1/2 w-full text-white p-4 font-medium">
-          Subscribe to Newsletter
-          {/* <div className="flex flex-col sm:flex-row sm:items-center gap-3 mt-6 w-full">
-            <input
-              type="email"
-              placeholder="Enter your email"
-              className="p-2 rounded-md bg-white text-black w-full sm:flex-1"
-            />
-
-            <button className="bg-white text-red-600 font-semibold px-4 py-2 rounded-md w-full sm:w-auto hover:shadow-xl hover:text-black transition">
-              Subscribe
-            </button>
-          </div> */}
-          <div className="flex flex-col sm:flex-row gap-3 mt-6 items-stretch">
-            <input
-              type="email"
-              placeholder="Enter your email"
-              className="flex-1 min-w-0 p-2 rounded-md bg-white text-black"
-            />
-
-            <button className="flex-none w-36 bg-white text-red-600 font-semibold px-4 py-2 rounded-md hover:shadow-xl hover:text-black transition">
-              Subscribe
-            </button>
+          <Image
+            src={ETHMumbaiFooterLogo}
+            alt="Twitter"
+            width={200}
+            height={100}
+            className="block"
+          />
+          <div className="flex flex-col gap-3 mt-6 items-stretch">
+            <h2 className="font-['MPlusRounded1c'] flex-1 text-2xl">
+              BEST Conference & Hackathon
+            </h2>
+            <p className="font-['MPlusRounded1c'] font-medium text-lg">
+              12-15 March 2026
+            </p>
           </div>
         </div>
 
-        {/* 1/4 width on desktop */}
-        <div className="sm:w-1/4 w-full lg:ml-30 p-4 text-white font-medium">
-          <p className="font-medium">About Us</p>
+        <div className="flex flex-row xl:gap-30 xl:px-10 2xl:gap-40 2xl:px-20 md:gap-10 md:px-5 gap-5">
+          {/* 1/4 width on desktop */}
+          <div className="p-4  text-white">
+            <p className="font-medium text-lg">About Us</p>
 
-          <div className="mt-6 flex flex-col gap-3">
-            <a
-              href="https://x.com/ethmumbai"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="group bg-[#E2231A] text-white font-normal w-fit
+            <div className="mt-6 flex flex-col gap-3 font-light">
+              <a
+                href="https://luma.com/ethmumbai"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="group bg-[#E2231A] text-white font-light w-fit hover:underline
 "
-            >
-              Luma
-            </a>
+              >
+                Luma
+              </a>
 
-            <a
-              href="https://x.com/ethmumbai"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="group bg-[#E2231A] text-white font-normal w-fit"
-            >
-              Media Kit
-            </a>
+              <a
+                href="https://drive.google.com/drive/folders/1Pz_gd9-Nqy8ol9t1vuDiGK7mOTf4wtJs"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="group bg-[#E2231A] text-white font-light w-fit hover:underline"
+              >
+                Brand Kit
+              </a>
 
-            <a
-              href="https://x.com/ethmumbai"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="group bg-[#E2231A] text-white font-normal w-fit"
-            >
-              ETHMumbai 2024
-            </a>
+              <a
+                href="https://2024.ethmumbai.in/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="group bg-[#E2231A] text-white font-light w-fit hover:underline"
+              >
+                ETHMumbai 2024
+              </a>
+
+              <a
+                href="https://ethmumbai.substack.com/embed"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="group bg-[#E2231A] text-white font-light w-fit hover:underline"
+              >
+                Subscribe to Newsletter
+              </a>
+            </div>
           </div>
-        </div>
 
-        {/* 1/4 width on desktop */}
-        <div className="sm:w-1/4 w-full lg:ml-30 p-4 text-white font-medium">
-          <p className="font-medium">Join Us</p>
+          {/* 1/4 width on desktop */}
+          <div className="p-4  text-white">
+            <p className="font-medium text-lg">Join Us</p>
 
-          <div className="mt-6 flex flex-col gap-3">
-            <a
-              href="https://tally.so/r/nGW5Bz"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="group bg-[#E2231A] text-white font-normal w-fit
+            <div className="mt-6 flex flex-col gap-3 font-light">
+              <a
+                href="https://tally.so/r/nGW5Bz"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="group bg-[#E2231A] text-white font-light w-fit hover:underline
 "
-            >
-              Apply to Speak
-            </a>
+              >
+                Apply to Speak
+              </a>
 
-            <a
-              href="https://x.com/ethmumbai"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="group bg-[#E2231A] text-white font-normal w-fit"
-            >
-              Apply to Sponsor
-            </a>
+              <a
+                href="https://tally.so/r/3NkdGb"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="group bg-[#E2231A] text-white font-light w-fit hover:underline"
+              >
+                Apply to Sponsor
+              </a>
 
-            <a
-              href="https://x.com/ethmumbai"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="group bg-[#E2231A] text-white font-normal w-fit"
-            >
-              Apply to Hack
-            </a>
-            <a
-              href="https://x.com/ethmumbai"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="group bg-[#E2231A] text-white font-normal w-fit"
-            >
-              Contact Us
-            </a>
+              <a
+                href="https://tally.so/r/3NkdGb"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="group bg-[#E2231A] text-white font-light w-fit hover:underline"
+              >
+                Apply to Hack
+              </a>
+              <a
+                href="mailto:hi@ethmumbai.in"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="group bg-[#E2231A] text-white font-light w-fit hover:underline"
+              >
+                Contact Us
+              </a>
+            </div>
           </div>
         </div>
       </div>
@@ -122,13 +125,6 @@ const Footer = () => {
       <div className="p-6 border-t flex flex-col sm:flex-row items-center sm:items-start justify-between gap-8 pb-10">
         {/* Left — Logo + trademark */}
         <div className="flex flex-row gap-3 items-center">
-          <Image
-            src={ETHMumbaiFooterLogo}
-            alt="Twitter"
-            width={20}
-            height={20}
-            className="block"
-          />
           <p className="text-medium text-white text-[14px] leading-6">
             © 2026 ETHMumbai. All rights reserved.
           </p>
@@ -140,7 +136,7 @@ const Footer = () => {
             href="https://x.com/ethmumbai"
             target="_blank"
             rel="noopener noreferrer"
-            className="group w-10 h-10 bg-[#E2231A] border-2 border-transparent flex items-center justify-center  transition-all duration-300 ease-in-out"
+            className="group w-10 h-10 bg-[#E2231A] border-2 border-transparent flex items-center justify-center transition-all duration-300 ease-in-out "
           >
             <Image
               src={TwitterWhite}
