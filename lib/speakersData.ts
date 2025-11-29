327dffd4f01c0fb107b1db93a4856135e88f7ac1
BEST developer <website@ethmumbai.in>
2025-11-29 15:29:49 +0530

    Merge Speakers section into develop (#23)
    * Merge Develop into main with animation of floating elements (#21)
    
    * chore: updates in navbar, hero
    
    * feat: update hamburger menu
    
    * chore: update buttons in hero
    
    * chore: update metadata and favicon
    
    * chore: partner assets
    
    * chore: vara asset update
    
    * fixes: small changes
    
    * feat: balloon cursor animation
    
    * vara
    
    * enhance: overview div position
    
    * fix: FAQ height
    
    * fix: overview
    
    * content update: faq and footer
    
    * fix: make hero content responsive
    
    * feat: responsive bus animation for sm, md, lg screens
    
    * fix: gap between content and bus in mobile screens
    
    * feat: add cityscape and make it responsive
    
    * feat: plane animation (#20)
    
    ---------
    
    Co-authored-by: Diksha <dikshas2911@gmail.com>
    
    * add form link to apply to speak, twitter link on image
    
    * add speakers image assets and change animation
    
    * make speaker section responsive
    
    * fix image alignment
    
    ---------
    
    Co-authored-by: Tanu Shree <tanushree.wrk@gmail.com>
    Co-authored-by: Diksha <dikshas2911@gmail.com>
    Co-authored-by: om santoshwar <omsantoshwar02@gmail.com>

--- lib/speakersData.ts
+++ lib/speakersData.ts
@@ -4,39 +4,45 @@ export interface Speaker {
     company: string;
     image: string;
     xLink?: string;
+    imageScale?: string;
 }
 
 export const speakers: Speaker[] = [
     {
+        name: "Denver Dsouza",
+        role: "Founder",
+        company: "Devfolio",
+        image: "/assets/speakers/Denver-Dsouza-removebg-preview.png",
+        xLink: "https://x.com/denverjude",
+        imageScale: "h-[140%]"
+    },{
         name: "Aditya Mohanty",
         role: "Founder",
         company: "The Product House",
         image: "/assets/speakers/aditya-mohanty.png",
-        xLink: "https://x.com/adityamohanty_"
+        xLink: "https://x.com/adityamohanty_",
+        imageScale: "h-[125%]"
     },{
         name: "Benny",
         role: "Founder",
         company: "The Product House",
         image: "/assets/speakers/benny.png",
-        xLink: "https://x.com/bennygiang"
+        xLink: "https://x.com/bennygiang",
+        imageScale: "h-[130%]"
     },{
         name: "CyberShakti",
         role: "Founder",
         company: "The Product House",
-        image: "/assets/speakers/CyberShakti-removebg-preview.png",
-        xLink: "https://x.com/CyberShakti"
-    },{
-        name: "Denver Dsouza",
-        role: "Founder",
-        company: "Devfolio",
-        image: "/assets/speakers/Denver-Dsouza-removebg-preview.png",
-        xLink: "https://x.com/denverjude"
+        image: "/assets/speakers/cybershakti.png",
+        xLink: "https://x.com/CyberShakti",
+        imageScale: "h-[130%]"
     },{
         name: "Gnana Lakshmi",
         role: "Founder",
         company: "The Product House",
         image: "/assets/speakers/Gnana-Lakshmi-removebg-preview.png",
-        xLink: "https://x.com/gyanlakshmi"
+        xLink: "https://x.com/gyanlakshmi",
+        imageScale: "h-[118%]"
     },{
         name: "Harsh Bajpai",
         role: "Founder",
@@ -54,8 +60,9 @@ export const speakers: Speaker[] = [
         name: "Hitesh Malviya",
         role: "Founder",
         company: "The Product House",
-        image: "/assets/speakers/Hitesh-Malviya-removebg-preview.png",
-        xLink: "https://x.com/hmalviya9"
+        image: "/assets/speakers/hitesh.png",
+        xLink: "https://x.com/hmalviya9",
+        imageScale: "h-[140%]"
     },{
         name: "Jason Goldberg",
         role: "Founder",
@@ -68,12 +75,6 @@ export const speakers: Speaker[] = [
         company: "The Product House",
         image: "/assets/speakers/Pareen-Lathia-removebg-preview.png",
         xLink: "https://x.com/PareenL"
-    },{
-        name: "Preet Parekh",
-        role: "Founder",
-        company: "The Product House",
-        image: "/assets/speakers/Preet-Parekh-removebg-preview.png",
-        xLink: "https://x.com/preetjdp"
     },{
         name: "Robin Roy",
         role: "Founder",
@@ -91,8 +92,9 @@ export const speakers: Speaker[] = [
         name: "Sahil sen",
         role: "Founder",
         company: "The Product House",
-        image: "/assets/speakers/Sahil-sen.png",
-        xLink: "https://x.com/sensahil"
+        image: "/assets/speakers/sahil-sen.png",
+        xLink: "https://x.com/sensahil",
+        imageScale: "h-[120%]"
     },{
         name: "Saravanan Vijayakumaran",
         role: "Founder",
@@ -105,6 +107,23 @@ export const speakers: Speaker[] = [
         role: "Founder",
         company: "The Product House",
         image: "/assets/speakers/Simone-removebg-preview.png",
-        xLink: "https://x.com/adityamohanty_"
+        xLink: "https://x.com/adityamohanty_",
+        imageScale: "h-[130%]"
     },
+    {
+        name: "Dhaiwat",
+        role: "Founder",
+        company: "The Product House",
+        image: "/assets/speakers/dhaiwat.png",
+        xLink: "https://x.com/dhaiwat10/",
+        imageScale: "h-[150%]"
+    },
+    {
+        name: "Preet Parekh",
+        role: "Founder",
+        company: "The Product House",
+        image: "/assets/speakers/Preet-Parekh-removebg-preview.png",
+        xLink: "https://x.com/preetjdp",
+        imageScale: "h-[175%]"
+    }
 ]
