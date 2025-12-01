664498f02a202ab1e0cab0eef48c808cca14fdad
Diksha <dikshas2911@gmail.com>
2025-12-01 16:15:53 +0530

    feat: payment

---
+++ app/buy-tickets/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import Hero from "@/components/buy-tickets/Hero";
+import Navbar from "@/components/Navbar";
+import FAQ from "@/components/FAQ";
+import Footer from "@/components/Footer";
+import Payment from "@/components/buy-tickets/Payment";
+
+export default function LandingPage() {
+  return (
+    <div className="flex min-h-screen flex-col bg-black font-sans">
+      <Navbar />
+      <main className="flex min-h-screen w-full flex-col">
+        <Hero />
+        {/* <FAQ /> */}
+        <Payment />
+        <Footer />
+      </main>
+    </div>
+  );
+}
