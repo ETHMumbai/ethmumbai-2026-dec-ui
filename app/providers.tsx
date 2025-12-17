c0c85bbcfffa22b1b26cc9fcc4ddf69b6507dfc5
Diksha <dikshas2911@gmail.com>
2025-12-17 23:32:42 +0530

    fix: provider

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,19 +1,23 @@
 "use client";
 
-import React from "react";
+import React, { useMemo } from "react";
 import { DaimoPayProvider, getDefaultConfig } from "@daimo/pay";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { WagmiProvider, createConfig } from "wagmi";
 
-const config = createConfig(
-  getDefaultConfig({
-    appName: "Daimo Pay Demo",
-  })
-);
-
 const queryClient = new QueryClient();
 
 export function Providers({ children }: { children: React.ReactNode }) {
+  const config = useMemo(
+    () =>
+      createConfig(
+        getDefaultConfig({
+          appName: "Daimo Pay Demo",
+        })
+      ),
+    []
+  );
+
   return (
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
