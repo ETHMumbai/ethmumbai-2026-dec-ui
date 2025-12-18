142eed4a6c8779200582cb82e42fc2db84b409c4
om santoshwar <omsantoshwar02@gmail.com>
2025-12-18 18:41:30 +0530

    add conference speakers

--- lib/speakersData.ts
+++ lib/speakersData.ts
@@ -116,5 +116,87 @@ export const pastSpeakers: Speaker[] = [
 ];
 
 export const conferenceSpeakers: Speaker[] = [
-  // TODO: Add conference speakers here
+  {
+    name: "Krishang Sha",
+    company: "Polygon Labs",
+    image: "/assets/speakers/Krishang_Shah.png",
+    xLink: "https://x.com/0xkrishang",
+    imageScale: "h-[130%]"
+  },
+  {
+    name: "Shiv Bhonde ",
+    company: "BuidlGuidl",
+    image: "/assets/speakers/Shiv_Bhonde.png",
+    xLink: "https://x.com/ShivBhonde",
+    imageScale: "h-[128%]"
+  },
+  {
+    name: "Aditi Polkam",
+    company: "Offline Protocol",
+    image: "/assets/speakers/Aditi_Polkam.png",
+    xLink: "https://x.com//aditipolkam",
+    imageScale: "h-[185%]"
+  },
+  {
+    name: "Jatin Pandya",
+    company: "Canton Foundation",
+    image: "/assets/speakers/Jatin_Pandya.png",
+    xLink: "https://twitter.com/Jpandya26",
+    imageScale: "h-[185%]"
+  },
+  {
+    name: "Dhaiwat",
+    company: "Turtle",
+    image: "/assets/speakers/dhaiwat.png",
+    xLink: "https://x.com/dhaiwat10",
+    imageScale: "h-[145%]"
+  },
+  {
+    name: "Sebastian Burkhard",
+    company: "Yodl Pay",
+    image: "/assets/speakers/Sebastian_Burkhard.png",
+    xLink: "https://x.com/yodl_meister",
+    imageScale: "h-[120%]"
+  },
+  {
+    name: "Kanika Mishra",
+    company: "USDT Marketplace",
+    image: "/assets/speakers/Kanika_Mishra.png",
+    xLink: "https://x.com/mishrakanika3",
+    imageScale: "h-[140%]"
+  },
+  {
+    name: "Rahat",
+    company: "Move Industries",
+    image: "/assets/speakers/Rahat.png",
+    xLink: "https://x.com/Rahatcodes",
+    imageScale: "h-[145%]"
+  },
+  {
+    name: "Ayush Bherwani",
+    company: "MetaMask",
+    image: "/assets/speakers/Ayush_Bherwani.png",
+    xLink: "http://x.com/ayushbherwani",
+    imageScale: "h-[120%]"
+  },
+  {
+    name: "Pareen Lathia",
+    company: "Monad Foundation",
+    image: "/assets/speakers/Pareen_Lathai.png",
+    xLink: "https://x.com/pareen"
+  },
+  {
+    name: "Rahul Bhadoriya",
+    company: "Dacoit",
+    image: "/assets/speakers/Rahul_Bhadoriya.png",
+    xLink: "https://x.com/rahulbhadoriya",
+    imageScale: "h-[130%]"
+  },
+  {
+    name: "Shyam Sridhar",
+    company: "Ethereum Foundation",
+    image: "/assets/speakers/Shyam_Sridhar.png",
+    xLink: "https://x.com/ShyamSridhar7",
+    imageScale: "h-[160%]"
+  }
 ];
