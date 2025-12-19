025b33a04c08feeea0f27614a58f42f51f846038
Tanu Shree <tanushree.wrk@gmail.com>
2025-12-19 20:44:37 +0530

    chore:prelauch changes #3

--- components/conference/Speakers.tsx
+++ components/conference/Speakers.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import { conferenceSpeakers } from "../../lib/speakersData";
+import { pastSpeakers } from "../../lib/speakersData";
 
 export default function Speakers() {
   return (
@@ -12,7 +12,7 @@ export default function Speakers() {
 
       {/* Speakers Grid */}
       <div className="px-4 sm:px-8 lg:px-12 flex flex-wrap justify-center gap-6 sm:gap-8 md:gap-12 mb-12 mx-auto">
-        {conferenceSpeakers.map((speaker, index) => (
+        {pastSpeakers.map((speaker, index) => (
           <div
             key={index}
             className="flex flex-col items-center w-[150px] sm:w-40 lg:w-[150px]"
