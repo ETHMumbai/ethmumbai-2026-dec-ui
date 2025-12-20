87a92b32717114f311be9833e63002aa84fed46c
BEST developer <website@ethmumbai.in>
2025-12-20 20:32:04 +0530

    Earlybird (#68)
    * chore: update ticker message
    
    * chore: update ticker message
    
    * chore: update quantity update
    
    * chore update form submission
    
    * chore update ticker
    
    * earlybird ticket
    
    * fixamount for earlybird
    
    * laucnh fix #1
    
    * fix:sold out alert
    
    * fix:ticker update
    
    * fix:ticket update
    
    ---------
    
    Co-authored-by: Tanu Shree <tanushree.wrk@gmail.com>

--- components/AnnouncementBar.tsx
+++ components/AnnouncementBar.tsx
@@ -15,40 +15,25 @@ export default function AnnouncementBar() {
       );
   }, []);
 
-  if (ticketCount === null) {
-    return null; // or a loading spinner
-  }
-  
-  // If ticketCount is 0, we can still show the bar but with a different message  
   return (
     <div className="fixed top-0 left-0 h-10 w-full z-[60] bg-[#FFD600] text-black px-3 py-2">
       <div className="flex items-center justify-center text-center">
         <span className="text-[12px] sm:text-sm md:text-base font-bold tracking-wide leading-tight sm:whitespace-nowrap">
-          Early Bird tickets are now available at ₹999.{" "}
-              <Link
-                href="/tickets"
-                className="underline underline-offset-2 hover:opacity-80 transition"
-              >
-                Buy now
-              </Link>{" "}
-              🤩
-          
-          {/* {ticketCount > 0 ? (
+          {ticketCount === null ? (
+            " "
+          ) : ticketCount > 0 ? (
             <>
-              Early Bird tickets are now available at ₹999.{" "}
+              {ticketCount} Early Bird tickets are now available at ₹999.{" "}
               <Link
                 href="/tickets"
                 className="underline underline-offset-2 hover:opacity-80 transition"
               >
                 Buy now
-              </Link>{" "}
-              🤩
+              </Link>
             </>
           ) : (
-            <>
-              Early bird tickets are sold out.{" "} 🎟️
-            </>
-          )} */}
+            "Early Bird tickets are sold out."
+          )}
         </span>
       </div>
     </div>
