323bec257413c19bbce53552760085b090f37f5e
Tanu Shree <tanushree.wrk@gmail.com>
2026-01-02 15:00:31 +0530

    chore: update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter_Tight } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next"
 import { Providers } from "./providers";
 import "./globals.css";
 
