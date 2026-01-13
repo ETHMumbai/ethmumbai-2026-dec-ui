5498610c6fe38e69129e5ac151eb63386d12a79a
Tanu Shree <tanushree.wrk@gmail.com>
2026-01-13 23:24:46 +0530

    feat:update venue design

--- lib/venueData.ts
+++ lib/venueData.ts
@@ -7,18 +7,21 @@ export interface VenueData {
   name: string;
   address: string;
   directionLink: string;
+  pincode?: string;
 }
 
 export const conferenceVenue: VenueData = {
   image: ConferenceImg,
   name: "Yashwantrao Chavan Centre",
-  address: "General Jagannath Bhosle Road, Nariman Point, Mumbai - 400021",
+  address: "General Jagannath Bhosle Road, Nariman Point,",
   directionLink: "https://maps.app.goo.gl/8sobRRBJxqF5ueqz6",
+  pincode: "400021",
 };
 
 export const hackathonVenue: VenueData = {
   image: HackathonImg,
    name: "World Trade Center",
-  address: "Ganesh Murti Nagar, Cuffe Parade, Mumbai - 400005",
+  address: "Ganesh Murti Nagar, Cuffe Parade,",
   directionLink: "https://maps.app.goo.gl/2eV6KSu1RPBnaFXaA",
+  pincode: "400005",
 };
