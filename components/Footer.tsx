85552d8546609493865953faa0a5a4e5bb5f42bb
Diksha <dikshas2911@gmail.com>
2026-03-04 08:03:13 +0530

    fix: footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -80,7 +80,7 @@ const Footer = () => {
                 rel="noopener noreferrer"
                 className="group bg-[#E2231A] text-white font-light w-fit hover:underline"
               >
-                  Download the Agenda App
+                  Download Agenda App
               </a>
               <a
                 href="https://ethmumbai.substack.com/embed"
