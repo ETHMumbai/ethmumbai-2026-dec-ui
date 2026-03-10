daa3dc86ed2956378a8df6b8b12c5910ba04c6da
Tanu Shree <tanushree.wrk@gmail.com>
2026-03-10 16:11:02 +0530

    update ethmumbai bounty

--- lib/bounties.ts
+++ lib/bounties.ts
@@ -184,6 +184,12 @@ export const currentBountyIcons: Bounty[] = [
     amount: "$2,000",
   },
   {
+    logo: "/assets/hackathon/bounties/currentLogos/ethmumbai.svg",
+    name: "ETHMumbai",
+    twitter: "https://x.com/ethmumbai",
+    amount: "$2,000",
+  },
+  {
     logo: "/assets/hackathon/bounties/currentLogos/heyelsa-icon.svg",
     name: "HeyElsa",
     twitter: "https://x.com/HeyElsaAI",
@@ -199,12 +205,6 @@ export const currentBountyIcons: Bounty[] = [
     logo: "/assets/hackathon/bounties/currentLogos/base.svg",
     name: "Base",
     twitter: "https://x.com/base",
-    amount: "$1,000",
-  },
-    {
-    logo: "/assets/hackathon/bounties/currentLogos/ethmumbai.svg",
-    name: "ETHMumbai",
-    twitter: "https://x.com/ethmumbai",
     amount: "$1,000",
   },
 ];
