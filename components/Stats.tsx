1aebbd32fc4ef209abd6e954d423c3f6363d32cb
Tanu Shree <tanushree.wrk@gmail.com>
2026-03-22 23:02:26 +0530

    Revert "update video size on mobile"
    This reverts commit 26c39874f873288459fc6a434f230e94c19d12e9.

--- components/Stats.tsx
+++ components/Stats.tsx
@@ -9,15 +9,14 @@ export default function Stats() {
     <section className="relative flex flex-col justify-center overflow-hidden bg-white border border-black text-white">
 
       {/* YouTube Video */}
-      <div className="w-full aspect-[16/10] md:aspect-[16/7] px-6 md:px-20 lg:px-32 xl:px-40 pt-8 pb-2 md:pt-20 md:pb-3 lg:pt-18 lg:pb-0">
-        <iframe
-          className="w-full h-full rounded-xl"
-          src="https://www.youtube.com/embed/_8ZoQDBC448?loop=1&playlist=_8ZoQDBC448&controls=1&modestbranding=1"
-          title="YouTube video player"
-          frameBorder="0"
-          allow="encrypted-media"
-          allowFullScreen
-        />
+<div className="w-full aspect-[16/7] px-6 md:px-20 lg:px-32 xl:px-40 pt-8 pb-2 md:pt-20 md:pb-3 lg:pt-18 lg:pb-0">        <iframe
+        className="w-full h-full rounded-xl"
+        src="https://www.youtube.com/embed/_8ZoQDBC448?loop=1&playlist=_8ZoQDBC448&controls=1&modestbranding=1"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="encrypted-media"
+        allowFullScreen
+      />
       </div>
 
       {/* Medium Screens */}
